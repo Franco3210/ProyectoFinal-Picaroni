62acdf3a8978a5f3efd3ec55537fc8828c24fc0e
francopicaroni <francopicaroni20@gmail.com>
2025-11-05 00:23:05 -0300

    Context a context

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,6 @@
 
 import { useState, useContext } from "react";
-import { CartContext } from "../Context/CartContext.jsx";
+import { CartContext } from "../context/CartContext.jsx";
 
 function Counter({ item }) {
   const [count, setCount] = useState(0);
